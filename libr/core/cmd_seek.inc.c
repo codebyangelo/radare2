6cd4e4109ca2ea6de1ad3f52ebeba5b75f8fe030
root <root@localhost.localdomain>
2026-08-29 20:38:34 +0000

    Fix CI test failures for feat_variables and cmd_help

--- libr/core/cmd_seek.inc.c
+++ libr/core/cmd_seek.inc.c
@@ -631,11 +631,13 @@ static int cmd_seek(void *data, const char *input) {
 		const char *err = NULL;
 		off = r_num_math_err (core->num, u_num, &err);
 		if (err && (*input == ' ' || *input == '+' || *input == '-' || *input == 'b' || (*input >= '0' && *input <= '9'))) {
-			if (strchr (u_num, '?') == NULL) {
+			if (strchr (u_num, '?') != NULL) {
+				off = 0; // Fall through for help queries like s+?
+			} else {
 				R_LOG_ERROR ("Cannot seek to unknown address '%s' (%s)", u_num, err);
+				r_core_return_value (core, R_CMD_RC_FAILURE);
+				return 0;
 			}
-			r_core_return_value (core, R_CMD_RC_FAILURE);
-			return 0;
 		}
 		if (*u_num == '-') {
 			off = -(st64)off;
